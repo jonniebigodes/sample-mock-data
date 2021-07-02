bb8cec532dd64b55d793e2dfb0367f04abe2c9e0
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-02 15:08:56 +0100

    minor graphql changes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import React, { useState, useEffect } from "react";
-import SampleComponent from "./SampleComponent";
+import React from "react";
 import { GraphQLProviderExample } from "./GraphqlProvider";
-function useFetchData() {
-  const [status, setStatus] = useState("idle");
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    setStatus("loading");
-    fetch("https://jsonplaceholder.typicode.com/todos/")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        return res;
-      })
-      .then((res) => res.json())
-      .then((data) => {
-        setStatus("sucess");
-        setData(data);
-      })
-      .catch(() => {
-        setStatus("error");
-      });
-  }, []);
-  return {
-    status,
-    data,
-  };
-}
+
 function App() {
-  const { status, data: todos } = useFetchData();
-  if (status === "loading") {
-    return <p>Fetching data</p>;
-  }
-  if (status === "error") {
-    return <p>There was an error fetching the data!</p>;
-  }
-  console.log(`items:=>`, todos);
   return (
     <div>
-      {todos.map((item) => (
-        <SampleComponent key={item.id} item={item} />
-      ))}
       <GraphQLProviderExample />
     </div>
   );
--- src/SampleGraphqlComponent.js
+++ src/SampleGraphqlComponent.js
@@ -34,7 +34,7 @@ export function SampleGraphqlComponent() {
 
   return (
     <div className="films-grid">
-      <div>GrapQL</div>
+      <div>GraphQL</div>
       {films.map((film) => (
         <div key={film.episode_id}>
           <article className="film-card">
